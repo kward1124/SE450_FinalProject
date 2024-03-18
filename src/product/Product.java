916eb28a79ebb333272cfaf8a412d4de37c5122c
Kevin Ward <126928229+kward1124@users.noreply.github.com>
2024-03-17 20:41:27 -0500

    Create Product.java

---
+++ src/product/Product.java
@@ -0,0 +1,14 @@
+package finalproject;
+
+public interface Product {
+
+    //Returns the name of the product
+    public String getName();
+
+    //Returns the price of the product
+    public double getPrice();
+
+    //Returns the category of the product
+    public String getCategory();
+
+} 
